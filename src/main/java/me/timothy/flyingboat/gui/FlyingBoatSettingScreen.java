27515bc713196c818c8917868862d1d393d47343
Timothy Julian <timothyace73@gmail.com>
2024-08-21 22:33:21 +0700

    upgrade to 1.21

--- src/main/java/me/timothy/flyingboat/gui/FlyingBoatSettingScreen.java
+++ src/main/java/me/timothy/flyingboat/gui/FlyingBoatSettingScreen.java
@@ -14,35 +14,18 @@ import net.minecraft.text.Text;
 
 public class FlyingBoatSettingScreen extends GameOptionsScreen {
 
-    private Screen previous;
     private OptionListWidget list;
 
     public FlyingBoatSettingScreen(Screen parent) {
         super(parent, MinecraftClient.getInstance().options, Text.translatable(TextKeyConstant.TITLE_MENU));
-        this.previous = parent;
     }
-
-    protected void init() {
-        this.list = new OptionListWidget(this.client, this.width, this.height, 32, this.height - 32, 25);
-        this.list.addAll(FlyingBoatConfig.asOptions());
-        this.addSelectableChild(this.list);
-        this.addDrawableChild(
-                ButtonWidget.builder(ScreenTexts.DONE, (button) -> {
-                            FlyingBoatConfigManager.save();
-                            if (this.client != null) {
-                                this.client.setScreen(this.previous);
-                            }
-                        }).position(this.width / 2 - 100, this.height - 27)
-                        .size(200, 20)
-                        .build());
-    }
-
 
     @Override
-    public void render(DrawContext DrawContext, int mouseX, int mouseY, float delta) {
-        super.render(DrawContext, mouseX, mouseY, delta);
-        this.list.render(DrawContext, mouseX, mouseY, delta);
-        DrawContext.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 5, 0xffffff);
+    protected void addOptions() {
+        if(this.body != null) {
+            this.body.addAll(FlyingBoatConfig.asOptions());
+        }
+
     }
 
     @Override
